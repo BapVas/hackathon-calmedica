57e53f4dc92b0585f7c193f360e916adb8115c9e
Baptiste Vasseur <baptiste.vasseur@calmedica.fr>
2024-05-31 11:50:04 +0200

    update prompt and remove ChatGPT in page title

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -21,8 +21,8 @@ export const categories: { [key: string]: CategoryDocument } = {
     score: null,
     responses: [],
   },
-  organisationnelle: {
-    name: 'organisationnelle',
+  organisationnel: {
+    name: 'organisationnel',
     score: null,
     responses: [],
   },
